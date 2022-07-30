909209c8236a7f139d3a8574fe3686af3725275a
FernandoDanielZigarra <99737822+FernandoDanielZigarra@users.noreply.github.com>
2022-07-30 17:37:42 -0300

    search added

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -2,17 +2,31 @@ import { useEffect, useState } from 'react';
 import { get } from '../utils/httpClient';
 import { MovieCard } from './MovieCard';
 import styles from './MoviesGrid.module.css';
-
+import { Spinner } from './Spinner';
+import { useQuery } from '../hooks/useQuery';
 export function MoviesGrid() {
 
     const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const query  = useQuery();
+    const search = query.get("search");
+
+
     useEffect(() => {
-        get("/discover/movie")
+        setIsLoading(true)
+        const searchURL = search ? "/search/movie?query=" + search : "/discover/movie";
+        get(searchURL)
             .then((data) => {
                 setMovies(data.results);
+                setIsLoading(false)
             });
 
-    },[]);
+    },[search]);
+
+    if(isLoading){
+        return <Spinner/>
+    }
     return (
         <ul className={styles.moviesGrid}>
             {movies.map((movie) => (
